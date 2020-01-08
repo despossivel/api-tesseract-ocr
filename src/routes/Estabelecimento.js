4416879c99cb14eef359c65c7ff61d1e15f83d92
matheus <matheus@ti>
2020-01-08 17:58:23 -0300

    removendo models

--- src/routes/Estabelecimento.js
+++ src/routes/Estabelecimento.js
@@ -1,5 +1,5 @@
 const { check } = require('express-validator');
-
+ 
 module.exports = (application) => {
 	const Estabelecimento = new application.src.controllers.Estabelecimento(application);
 
@@ -13,8 +13,22 @@ module.exports = (application) => {
 		application.src.middlewares.Jwt.verify,
 		check('nome').notEmpty(),
 		check('nomeFantasia').notEmpty(),
-		check('cnpj').notEmpty(),
-		check('email').notEmpty().isEmail(),
+
+		check('cnpj').notEmpty().custom((value) => {
+			return this.src.models.Estabelecimento.findOne({ cnpj: value }).then(cnpj=>{
+				 if(cnpj){
+					 return Promise.reject('Cnpj já está em uso')
+				 }
+			 })
+	   }),
+ 
+		check('email').notEmpty().isEmail().custom((value) => {
+			return this.src.models.Estabelecimento.findOne({ email: value }).then(email=>{
+				 if(email){
+					 return Promise.reject('E-mail já está em uso')
+				 }
+			 })
+	   }),
 		check('senha').notEmpty()
 	], (req, res) => Estabelecimento.store(req, res))
 
--- src/routes/Usuario.js
+++ src/routes/Usuario.js
@@ -1,5 +1,5 @@
 const { check } = require('express-validator');
-
+ 
 module.exports = (application) => {
 	const Usuario = new application.src.controllers.Usuario(application);
 
@@ -13,12 +13,38 @@ module.exports = (application) => {
 	application.post('/usuario/store', [
 		application.src.middlewares.Jwt.verify,
 		check('nome').notEmpty(),
-		check('usuario').notEmpty(),
-		check('email').notEmpty().isEmail(),
+
+
+		check('usuario').notEmpty().custom((value) => {
+			return this.src.models.Usuario.findOne({ usuario: value }).then(usuario=>{
+				 if(usuario){
+					 return Promise.reject('Nome de usuário já está em uso')
+				 }
+			 })
+	   }),
+ 
+		check('email').notEmpty().isEmail().custom((value) => {
+			 return this.src.models.Usuario.findOne({ email: value }).then(email=>{
+				  if(email){
+					  return Promise.reject('E-mail já está em uso')
+				  }
+			  })
+		}),
+
 		check('cidade').notEmpty(),
 		check('estado').notEmpty().isLength({ max: 2 }),
-		check('senha').notEmpty().isLength({ min: 5 }),
-		check('telefone').notEmpty()
+		check('senha').notEmpty().isLength({ min: 5 }).withMessage('deve ter pelo menos 5 caracteres'),
+
+
+		check('telefone').notEmpty().custom((value) => {
+			return this.src.models.Usuario.findOne({ telefone: value }).then(telefone=>{
+				 if(telefone){
+					 return Promise.reject('Telefone já está em uso')
+				 }
+			 })
+	   })
+
+
 	], (req, res) => Usuario.store(req, res))
 
 	application.put('/usuario/update', [
