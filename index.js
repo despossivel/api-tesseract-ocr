5ab4c3593b8ba196d7173b8879513d3ce0e87a18
matheus <matheus@ti>
2020-01-11 11:06:42 -0300

    metodos de pagamentos

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 const app = require('./src/server');
 const PORT = 5000;
- 
-app.listen(process.env.PORT || PORT, ()=>console.log(`SERVER ON-LINE NA PORTA ${PORT}`));
+
+app.listen(process.env.PORT || PORT, () => console.log(`SERVER ON-LINE NA PORTA ${PORT}`));
--- src/routes/MetodosDePagamento.js
+++ src/routes/MetodosDePagamento.js
@@ -9,7 +9,7 @@ module.exports = (application) => {
 
    application.get('/metodo/pagamento/:cvv/:_id', [
       application.src.middlewares.Jwt.verify,
-      check('cvv').notEmpty(),
+      check('cvv').notEmpty().isLength({ min: 3, max:3 }),
       check('_id').notEmpty(),
       ], (req, res) => MetodosDePagamento.show(req, res))
 
@@ -17,10 +17,10 @@ module.exports = (application) => {
       application.src.middlewares.Jwt.verify,
       check('_idUsuario').notEmpty(),
       check('tipo').notEmpty(),
-      check('titular').notEmpty(),
-      check('numero').notEmpty(),
-      check('mes').notEmpty(),
-      check('ano').notEmpty(),
+      check('titular').notEmpty().isLength({ min: 5 }),
+      check('numero').notEmpty().isLength({ min: 14, max:16 }),
+      check('mes').notEmpty().isLength({ min: 1, max:2 }),
+      check('ano').notEmpty().isLength({ min: 2, max:2 }),
    ], (req, res) => MetodosDePagamento.store(req, res))
 
    application.put('/metodo/pagamento/update', [
