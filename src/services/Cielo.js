8ef599b635ae83f84233965cd560df881648e026
despossivel <mattbmoller@gmail.com>
2020-01-12 18:24:59 -0300

    service onesignal

--- src/services/Cielo.js
+++ src/services/Cielo.js
@@ -4,44 +4,49 @@ class Cielo {
 		this.GATEWAY = application.src.config.cielo;
 	}
 
-	async payment(card, payment){
-	 
+	async payment(card, payment) {
+
 		const dadosSale = {
-					"MerchantOrderId":  Math.random() * (9999 - 1) + 123,
-					"Customer":{
-					   "Name":"Centavus Leilões Online"
-					},
-					"Payment":{
-					  ...payment,
-					  "ReturnUrl":"http://www.cielo.com.br",
-					  "Installments":1,
-					  "SoftDescriptor":"Centavus",
-					  [payment.Type]:{
-						  ...card,
-						  "CardOnFile":{
-							 "Usage": "Used",
-							 "Reason":"Unscheduled"
-						  }
-					  }
+			"MerchantOrderId": Math.random() * (9999 - 1) + 123,
+			"Customer": {
+				"Name": "Centavus Leilões Online"
+			},
+			"Payment": {
+				...payment,
+				"ReturnUrl": "http://www.cielo.com.br",
+				"Installments": 1,
+				"SoftDescriptor": "Centavus",
+				[payment.Type]: {
+					...card,
+					"CardOnFile": {
+						"Usage": "Used",
+						"Reason": "Unscheduled"
 					}
-				 };
+				}
+			}
+		};
 
-				 switch(payment.Type){
-					 case 'CreditCard':
-						 	return await this.credit(dadosSale).catch(e=>console.error(e));
-						 break;
+		switch (payment.Type) {
+			case 'CreditCard':
+				return await this.credit(dadosSale).catch(e => console.error(e));
+				break;
 
-					case 'DebitCard':
-						return await this.debit(dadosSale).catch(e=>console.error(e));
-						break;
-				 }
+			case 'DebitCard':
+				return await this.debit(dadosSale).catch(e => console.error(e));
+				break;
+		}
 	}
 
 	credit = async (sale) => await this.GATEWAY.creditCard.completeTransaction(sale)
-																.catch(error=>console.error(error));
+		.catch(error => console.error(error));
 
-	debit = async (sale) =>  await this.GATEWAY.debitCard.simpleTransaction(sale)
-																.catch(error=>console.error(error));
+	debit = async (sale) => await this.GATEWAY.debitCard.simpleTransaction(sale)
+		.catch(error => console.error(error));
+
+
+	consulting = async (paymentId) => await this.GATEWAY.consulting.sale(paymentId)
+		.catch(e => console.error(e));
+
 
 }
 
---
+++ src/services/Onesignal.js
@@ -0,0 +1,46 @@
+const https = require('https');
+
+
+class Onesignal {
+
+    constructor() {
+
+        this.OPTIONS = {
+            host: "onesignal.com",
+            port: 443,
+            path: "/api/v1/notifications",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Authorization": `Basic ${process.env.ONESIGNAL_AUTHORIZATION ? process.env.ONESIGNAL_AUTHORIZATION : 'MTQyNDhkY2MtOWI4MC00M2M0LTgyZGMtMjM1MDAzZGFlMjVj'}`
+            }
+        }
+
+    }
+
+    async send(title = 'teste',
+                msg = 'ola, isso é um teste',
+                include_player_ids = ['51d1a20c-006f-4a92-9454-5a861aa3a206']) {
+
+        const data = {
+            app_id: process.env.ONESIGNAL_APP_ID ? process.env.ONESIGNAL_APP_ID : 'cb9f46a8-08be-48e9-86a2-da5b880cd58f',
+            include_player_ids,
+            headings: { "en": title },
+            contents: { "en": msg },
+            small_icon: "ic_stat_onesignal_default",
+            large_icon: "ic_stat_onesignal_default"
+        };
+
+        const req = https.request(this.OPTIONS, (res) => res.on(data, (data) => console.log(JSON.parse(data))));
+
+        req.on('error', (e) => console.error("ERROR:", e));
+        req.write(JSON.stringify(data));
+        req.end();
+
+    }
+
+}
+
+//const onesignal = new Onesignal();
+//const send = onesignal.send()
+module.exports = () => new Onesignal();
