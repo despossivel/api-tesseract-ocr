a1102989bfd8d360fb1c30b63ebf62a9e1d4d8c6
matheus <matheus@ti>
2020-02-20 10:35:23 -0300

    view ejs

--- src/server.js
+++ src/server.js
@@ -4,16 +4,21 @@ const bodyParser = require('body-parser');
 const env = require('dotenv').config()
 const cors = require('cors');
 const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'))
+
 app.use('/static', express.static('./public/uploads'))
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
- 
+
 consign()
     .then('src/controllers')
-    .then('src/models') 
+    .then('src/models')
     .then('src/services')
     .then('src/middlewares')
     .then('src/config')
