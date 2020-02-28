eb40c3c40cefc04065bfc7e3e33b14705bea367b
matheus <matheus@ti>
2020-02-28 17:17:13 -0300

    problema o check de usuario

--- src/middlewares/routes/usuario.js
+++ src/middlewares/routes/usuario.js
@@ -16,30 +16,30 @@ module.exports = {
     store: [
         //  Jwt.verify,
         check('nome').notEmpty(),
-        check('usuario').notEmpty().custom((value) => {
-            return Model.findOne({ usuario: value }).then(usuario => {
-                if (usuario) {
-                    return Promise.reject('Nome de usuário já está em uso')
-                }
-            })
-        }),
-        check('email').notEmpty().isEmail().custom((value) => {
-            return Model.findOne({ email: value }).then(email => {
-                if (email) {
-                    return Promise.reject('E-mail já está em uso')
-                }
-            })
-        }),
+        // check('usuario').notEmpty().custom((value) => {
+        //     return Model.findOne({ usuario: value }).then(usuario => {
+        //         if (usuario) {
+        //             return Promise.reject('Nome de usuário já está em uso')
+        //         }
+        //     })
+        // }),
+        // check('email').notEmpty().isEmail().custom((value) => {
+        //     return Model.findOne({ email: value }).then(email => {
+        //         if (email) {
+        //             return Promise.reject('E-mail já está em uso')
+        //         }
+        //     })
+        // }),
         check('municipio').notEmpty(),
         check('estado').notEmpty().isLength({ max: 2 }),
         check('senha').notEmpty().isLength({ min: 5 }).withMessage('Sua senha deve ter pelo menos 5 caracteres'),
-        check('telefone').notEmpty().custom((value) => {
-            return Model.findOne({ telefone: value }).then(telefone => {
-                if (telefone) {
-                    return Promise.reject('Telefone já está em uso')
-                }
-            })
-        }),
+        // check('telefone').notEmpty().custom((value) => {
+        //     return Model.findOne({ telefone: value }).then(telefone => {
+        //         if (telefone) {
+        //             return Promise.reject('Telefone já está em uso')
+        //         }
+        //     })
+        // }),
         expressValidation.validation
     ],
     update: [
