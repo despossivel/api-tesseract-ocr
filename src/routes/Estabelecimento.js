dba0a455c2aea8c3c78a236f370ce02e07f82ebd
matheus <matheus@ti>
2020-02-29 09:27:00 -0300

    dev

--- src/routes/Estabelecimento.js
+++ src/routes/Estabelecimento.js
@@ -7,7 +7,23 @@ module.exports = (application) => {
 
 	application.get('/estabelecimento/:_id', application.src.middlewares.routes.estabelecimento.show, (req, res) => Estabelecimento.show(req, res))
 
-	application.post('/estabelecimento/store', application.src.middlewares.routes.estabelecimento.store, (req, res) => Estabelecimento.store(req, res))
+	application.post('/estabelecimento/store',
+
+		// application.src.middlewares.routes.estabelecimento.store
+		[
+			application.src.middlewares.Jwt.verify,
+			check('nome').notEmpty(),
+			check('nomeFantasia').notEmpty(),
+			check('cnpj').notEmpty().custom(async (value) => {
+				const cnpj = application.src.models.Estabelecimento.findOne({ cnpj: value })
+				if (cnpj) {
+					throw new Error('Cnpj já está em uso')
+				}
+			}),
+			check('_idUsuario').notEmpty(),
+			application.src.middlewares.expressValidation.validation
+		]
+		, (req, res) => Estabelecimento.store(req, res))
 
 	application.put('/estabelecimento/update', application.src.middlewares.routes.estabelecimento.update, (req, res) => Estabelecimento.update(req, res))
 
