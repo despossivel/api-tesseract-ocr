19cde6ca10379313577e1ba02390de6a484c28ee
despossivel <mattbmoller@gmail.com>
2020-03-08 21:31:34 -0300

    quase finalizado

--- src/routes/CartaoFidelidade.js
+++ src/routes/CartaoFidelidade.js
@@ -8,9 +8,10 @@ route.get('/cartoes/fidelidade', middleware.index, CartaoFidelidade.index)
 route.get('/cartao/fidelidade/:_id', middleware.show, CartaoFidelidade.show)
 route.post('/cartao/fidelidade', middleware.store, CartaoFidelidade.store)
 
-//route.put('/cartao/fidelidade/:_id', middleware.update, CartaoFidelidade.update)
-route.put('/cartao/fidelidade/:_idEstabelecimento/:_idUsuario', middleware.update, CartaoFidelidade.update)
 
+route.put('/cartao/fidelidade/:_id', middleware.update, CartaoFidelidade.update)
+
+route.put('/cartao/fidelidade/:_idEstabelecimento/:_idUsuario', middleware.update, CartaoFidelidade.update)
 route.put('/cartao/fidelidade/cpf/:_idEstabelecimento/:cpf', middleware.update, CartaoFidelidade.update)
 
 
