762f714a355e3ed83ddb55a581cbfbf1a6f43dd9
despossivel <mattbmoller@gmail.com>
2020-06-03 21:50:46 -0300

    msg usuario nao confirmado

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -8,19 +8,21 @@ class Auth {
 
 	async show(req, res, next) {
 		const { email, senha } = req.body;
- 
+
 
 		const token = JWT.sing({});
 		const senhaEncrypt = blowfish.encrypt(senha)
-		const login = await Model.findOne({ email, senha: senhaEncrypt, status: true }).catch(e => console.log(e))
+		const login = await Model.findOne({ email, senha: senhaEncrypt }).catch(e => console.log(e)) //, status: true
 		let response;
+
+		if (login && !login._doc.status) response = { errors: [{ "msg": "Usuario não confirmado, verifique seu e-mail!" }], status: 401 };
 
 		login ? response = {
 			...login._doc,
 			fotoUrl: login.fotoUrl,
 			token
 		} : {};
-  
+
 
 		login ? res.status(200).send(response)
 			: res.status(404).send({ errors: [{ "msg": "Usuario não encontrado!" }], status: 404 })
