0abfe8b1727e828833eca03daa87cca0ae69ab8f
Matheus Henrique Sousa Brito <despossivel@despossivel.local>
2021-07-01 20:28:24 -0300

    feature/pricePerKm

---
+++ src/models/PricePerKm.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose'),
+    ObjectId = mongoose.Schema.Types.ObjectId;
+
+
+
+
+const getPrice = num => (num / 100).toFixed(2);
+
+const setPrice = num => parseFloat(num) * 100;
+
+
+const PricePerKm = new mongoose.Schema({
+    de: {
+        type: Number,
+        require: true
+    },
+    ate: {
+        type: Number,
+        require: true
+    },
+    price: {
+        type: Number,
+        get: getPrice,
+        set: setPrice,
+        require: true
+    }
+},
+    {
+        timestamps: true,
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true
+    });
+
+module.exports = mongoose.model('PricePerKm', PricePerKm);
